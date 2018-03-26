6224e322a2aa09dc96c6c494cb5f693728318d3a
David Robles <drobles@gmail.com>
2018-03-25 18:05:18 -0700

    Run functions

--- chimera/block_generators.py
+++ chimera/block_generators.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from chimera.block import Block
+from chimera.tile import TileType
 
 
 class BlockGenerator(ABC):
@@ -31,3 +32,34 @@ class RandomGridBlockGenerator(BlockGenerator):
                     r.append(block_type.generate_tile())
             tiles.append(r)
         return Block(block_type=block_type, tiles=tiles)
+
+
+class ProbabilisticGridBlockGenerator(BlockGenerator):
+    """Independent Probabilistic Block Generator"""
+
+    def __init__(self, dims, probs):
+        self.dims = dims
+        self.probs = probs
+
+    def generate(self, block_type):
+        print('Generating block using RandomGridBlockGenerator...')
+        tiles = []
+
+        def is_border(row, col):
+            return row == 0 or row == self.dims[0] - 1 or col == 0 or col == self.dims[1] - 1
+
+        for row in range(self.dims[0]):
+            r = []
+            for col in range(self.dims[1]):
+                if is_border(row, col):
+                    r.append(block_type.generate_blocking_tile())
+                else:
+                    r.append(block_type.generate_tile())
+            tiles.append(r)
+        return Block(block_type=block_type, tiles=tiles)
+
+
+class EmptyGrid(ProbabilisticGridBlockGenerator):
+    def __init(self, dims, tile_type):
+        probs = {tile_type: 1.0}
+        super().__init__(dims, probs)
--- chimera/main.py
+++ chimera/main.py
@@ -1,6 +1,24 @@
-from chimera.block_generators import RandomGridBlockGenerator
+from chimera.block_generators import RandomGridBlockGenerator, ProbabilisticGridBlockGenerator
 from chimera.sector_generators import GridSectorGenerator
+from chimera.tile import TileType
+
+def run_random_generator():
+    block_generator = RandomGridBlockGenerator((15, 15))
+    sector = GridSectorGenerator(dims=(3, 5), block_generator=block_generator).generate()
+    print(sector)
+
+
+def run_probabilistic_generator():
+    probs = {
+        TileType.GRASS: 0.80,
+        TileType.ROCK: 0.10,
+        TileType.WATER: 0.10,
+    }
+    block_generator = ProbabilisticGridBlockGenerator(dims=(3, 5), probs=probs)
+    sector = GridSectorGenerator(dims=(3, 5), block_generator=block_generator).generate()
+    print(sector)
+
 
 if __name__ == '__main__':
-    sector = GridSectorGenerator(dims=(3, 5), block_generator=RandomGridBlockGenerator((15, 15))).generate()
-    print(sector)
+    run_random_generator()
+    # run_probabilistic_generator()
